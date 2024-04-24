57b6e66e5846fac3671727203b61ad7329a95e5e
Harsh Butani <harshbutani2004@gmail.com>
2024-04-24 11:30:52 +0530

    git init by divy247

---
+++ Frontend/lib/widgets/dashboard_widgets/section1.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DashboardSection1 extends StatelessWidget {
+  const DashboardSection1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const Row(),
+        Image.asset(name),
+      ],
+    );
+  }
+}
